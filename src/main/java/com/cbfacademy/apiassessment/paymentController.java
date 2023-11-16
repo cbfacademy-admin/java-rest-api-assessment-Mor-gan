ee031f01f997d2c807f101b63339d8c4acdc585a
Mor-gan <ogundipeabimbola9@gmail.com>
2023-11-16 16:49:29 +0000

    Exception handling and create payment update

--- src/main/java/com/cbfacademy/apiassessment/paymentController.java
+++ src/main/java/com/cbfacademy/apiassessment/paymentController.java
@@ -1,6 +1,5 @@
 package com.cbfacademy.apiassessment;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cbfacademy.filehandler.InsufficientBalanceException;
